492935f21920b56ff669dba060002d24ee94215e
fmiglior <50951828+fmiglior@users.noreply.github.com>
2021-02-08 11:03:54 -0300

    Update ats_dash_v2.ino
    code update, remove unused variables

--- ats_dash_v2/ats_dash_v2.ino
+++ ats_dash_v2/ats_dash_v2.ino
@@ -1,3 +1,4 @@
+//fmr
 #include <SPI.h>
 #include <mcp2515.h>
 #include "cancode.h"
@@ -5,10 +6,10 @@
 
 
 
-MCP2515 mcp2515(10);
+MCP2515 mcp2515(10); //CS pin on arduino
 
 
-unsigned long prev_tempo_1s;
+
 #define PACKET_SYNC 0xFF
 #define PACKET_VER  2
 
@@ -18,10 +19,10 @@ void setup() {
   Serial.begin(115200);
 
     mcp2515.reset();
-  mcp2515.setBitrate(CAN_125KBPS,MCP_8MHZ);
+  mcp2515.setBitrate(CAN_125KBPS,MCP_8MHZ); //mcp2515 clock
   mcp2515.setNormalMode();
 
-carregaCAN();
+carregaCAN(); //load can msg pack
 
 }
 
@@ -44,7 +45,7 @@ mcp2515.sendMessage(&canMsg6);
    serial_byte = Serial.read();
    if (serial_byte != PACKET_VER)
    return;
-  
+//game data variables  
 int speed_kph =                Serial.read();
 int speed_decimal =            Serial.read();
 int lsb_rpm =                  Serial.read();
@@ -92,8 +93,6 @@ int low_beam =                 ((OCTET_0 >> 3) & 0x01);
 int high_beam =                ((OCTET_0 >> 2) & 0x01);
 int park_brake =               ((OCTET_0 >> 1) & 0x01);
 int temperature_warning =      ((OCTET_1 >> 0) & 0x01);
-int trailer_connected =        ((OCTET_2 >> 6) & 0x01);
-int retarder =                 ((OCTET_2 >> 4) & 0x01);
 int electric_enabled =         ((OCTET_2 >> 1) & 0x01);
 int engine_enabled =           ((OCTET_2 >> 0) & 0x01);
 
@@ -101,8 +100,7 @@ int lsb;
 int msb;
 int lsb1;
 int msb1;
-int split_low;
-int retarder_state;
+
 
 int rpm = msb_rpm;
 rpm = rpm <<8;
@@ -112,12 +110,8 @@ rpm =                             rpm*0.031286;
 speed_kph =                       speed_kph*0.390909;
 speed_decimal =                   map(speed_decimal, 0, 5, 0, 255);
 water_temperature =               map(water_temperature, 50, 101,  101, 131);
-air_pressure =                    map(air_pressure, 0, 140, 0, 150);
 fuel =                            map(fuel, 0, 100, 0, 128);
-oil_pressure =                    map(oil_pressure, 0, 86, 0, 175);
-int turbo =                       map(effective_throttle, 0, 100, 0, 75);
-int instant_consuption_lh =       map(effective_throttle, 0, 100, 0, 96);
-int instant_consuption_lk =       map(effective_throttle, 0, 100, 64254, 427);
+
 
 long odometer = odometer_4;
 odometer = odometer << 8;
@@ -128,12 +122,12 @@ odometer = odometer << 8;
 odometer = odometer + odometer_1;
 odometer = odometer*200;
 
-//ajusta can
+//convert variables to can msg
 //rpm
 canMsg3.data[0] = (rpm & 0xFF);
 //speed
- canMsg3.data[2] = (speed_kph & 0xFF);
- canMsg3.data[3] = (speed_decimal & 0xFF);
+canMsg3.data[2] = (speed_kph & 0xFF);
+canMsg3.data[3] = (speed_decimal & 0xFF);
 //water temp
 canMsg1.data[1] = (water_temperature  &0xFF);
 //blinker + headlight
@@ -143,7 +137,6 @@ canMsg4.data[4] = (((left_blinker*2)+(right_blinker*4)+(high_beam*32)+(low_beam*
 //ignition
 canMsg1.data[0] = ((electric_enabled*142)&0xFF);
 //fuel
-
 canMsg6.data[3] = (fuel &0xFF);
 
 
